5115ce339f44593d9292e8121c9b49e4f84fe276
persianace <nimastar666@gmail.com>
2020-11-11 23:54:12 -0800

    firebase-node-express

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,11 @@ const app = expree();
 app.get('/time', (req, res)=>{
     res.send(`${Date.now()}`);
 })
+// cache data 
+app.get('/timecache', (req, res)=>{
+    res.set('Cache-control', 'public, max-age=300, s-maxage=600');
+    res.send(`${Date.now()}`);
+})
 
 exports.app = functions.https.onRequest(app 
     //=> {
